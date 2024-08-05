42d618d1b33ec9e37c7fc84e1b29f6a987242d9a
fengsim <jiping.xin@lsec.cc.ac.cn>
2024-08-05 16:22:38 +0800

    c

--- mfem/install.sh
+++ mfem/install.sh
@@ -11,15 +11,17 @@ cd build
 cmake \
     -DMFEM_USE_CEED=ON \
     -DMFEM_USE_MPI=ON \
-    -DMFEM_USE_SUPERLU5=ON \
+    -DMFEM_USE_SUPERLU=ON \
     -DMFEM_USE_METIS=ON \
     -DCEED_DIR=$PWD/../../../NLA/install/libceed_install \
     -DHYPRE_DIR=$PWD/../../../NLA/install/hypre_install \
     -DParMETIS_DIR=$PWD/../../../install/parmetis_install \
-    -DSuperLUDist_DIR=$PWD/../../../NLA/install/superlu_install_dist \
-    -DSuperLUDist_INCLUDE_DIR=$PWD/../../../NLA/install/superlu_install_dist/include \
-    -DSuperLUDist_LIBRARY=$PWD/../../../NLA/install/superlu_install_dist/lib/libsuperlu_dist.a \
     -DMETIS_DIR=$PWD/../../../NSM/extern/ALE/install/metis \
+    -DSuperLUDist_DIR=$PWD/../../../NLA/install/superlu_dist_install \
+    -DSuperLUDist_INCLUDE_DIR=$PWD/../../../NLA/install/superlu_dist_install/include \
+    -DSuperLUDist_LIBRARY=$PWD/../../../NLA/install/superlu_dist_install/lib/libsuperlu_dist.a \
     -DCMAKE_INSTALL_PREFIX=$PWD/../../install/mfem_install .. 
 make -j4
 make install
+
+
