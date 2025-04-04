8aa973a6f7f2dce177dd798d9d15b6109b10043f
fengsim <jiping.xin@lsec.cc.ac.cn>
2025-04-04 12:51:14 +0800

    c

--- test-docker.sh
+++
@@ -1,5 +0,0 @@
-sudo docker rm test
-sudo docker run --name test -it -v $HOME/FENGSim:/test ubuntu:24.04 /bin/bash
-#sudo docker run --name test -it -v $HOME/Downloads:/test ubuntu:24.04 /bin/bash
-#sudo docker run --name test -it -v $HOME/Downloads:/test ros2-ubuntu22.04 /bin/bash
-
